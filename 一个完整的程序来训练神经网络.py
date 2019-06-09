501f87422386e935579fcf694075963d124772af
mrwtx1618 <742750341@qq.com>
2019-06-09 12:13:21 +0800

    first commit

---
+++ 一个完整的程序来训练神经网络.py
@@ -0,0 +1,37 @@
+#encoding:utf-8
+
+import tensorflow as tf
+from numpy.random import RandomState
+from sklearn import datasets
+
+print(dir(datasets))
+
+
+diabetes = datasets.load_diabetes()
+print(diabetes.data)
+
+
+import numpy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
